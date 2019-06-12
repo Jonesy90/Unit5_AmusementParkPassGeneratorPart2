a9a0b6591433c3b72c600a28aa8dbef5769f4a08
Michael Jones <michael.jones90@me.com>
2019-06-12 20:38:56 +0100

    Initial Commit

---
+++ Unit5_AmusementParkPassGeneratorPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit5_AmusementParkPassGeneratorPart2
+//
+//  Created by Michael Jones on 12/06/2019.
+//  Copyright © 2019 Michael Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit5_AmusementParkPassGeneratorPart2Tests/Unit5_AmusementParkPassGeneratorPart2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Unit5_AmusementParkPassGeneratorPart2Tests.swift
+//  Unit5_AmusementParkPassGeneratorPart2Tests
+//
+//  Created by Michael Jones on 12/06/2019.
+//  Copyright © 2019 Michael Jones. All rights reserved.
+//
+
+import XCTest
+@testable import Unit5_AmusementParkPassGeneratorPart2
+
+class Unit5_AmusementParkPassGeneratorPart2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
