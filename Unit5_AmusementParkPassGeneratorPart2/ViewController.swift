458f832db90310cb646563e4d7c9e2b1ffce29f3
Michael Jones <michael.jones90@me.com>
2019-06-20 19:30:38 +0100

    - DatePicker added.

--- Unit5_AmusementParkPassGeneratorPart2/ViewController.swift
+++ Unit5_AmusementParkPassGeneratorPart2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //UITextFields & DatePicker
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    private var datePicker: UIDatePicker?
+    
+    
+    
     //UIButtons
     @IBOutlet weak var guestButton: UIButton!
     @IBOutlet weak var employeeButton: UIButton!
@@ -20,8 +26,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //When the user presses on the 'dateOfBirthTextField'. A DatePicker will appear.
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(ViewController.dateChanged(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.viewTapped(gestureRecongnizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        dateOfBirthTextField.inputView = datePicker
+        
+        
     }
+    
+    //will remove the datepicker once the user has finished.
+    @objc func viewTapped(gestureRecongnizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    //updates the input of the date in the text field and assigns a date format.
+    @objc func dateChanged(datePicker: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        dateOfBirthTextField.text = dateFormatter.string(from: datePicker.date)
+    }
+
     
     
     
