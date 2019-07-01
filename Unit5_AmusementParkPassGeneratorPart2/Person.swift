ba56ee168768a8a6157fabd492f379489da35766
Michael Jones <michael.jones90@me.com>
2019-07-01 21:40:16 +0100

    - Finished updating the 'Person' Swift file.

--- Unit5_AmusementParkPassGeneratorPart2/Person.swift
+++ Unit5_AmusementParkPassGeneratorPart2/Person.swift
@@ -34,15 +34,44 @@ class Guest: AreaAcces {
 }
 
 class FreeChildGuest: Guest {
+    var dateOfBirthString: String //Must be under the age of 5 years old.
     
+    init(dateOfBirth: String) {
+        dateOfBirthString = dateOfBirth
+    }
 }
 
 class SeasonPassGuest: Guest {
+    var firstNameString: String
+    var lastNameString: String
+    var streetAddressString: String
+    var cityString: String
+    var stateString: String
+    var zipCodeString: String
+    var dateOfBirthString: String
+    
+    init(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String, dateOfBirth: String) {
+        firstNameString = firstName
+        lastNameString = lastName
+        streetAddressString = streetAddress
+        cityString = city
+        stateString = state
+        zipCodeString = zipCode
+        dateOfBirthString = dateOfBirth
+    }
     
 }
 
 class SeniorGuest: Guest {
+    var firstNameString: String
+    var lastNameString: String
+    var dateOfBirthString: String //Needs to do some sort of check.
     
+    init(firstName: String, lastName: String, dateOfBirth: String) {
+        firstNameString = firstName
+        lastNameString = lastName
+        dateOfBirthString = dateOfBirth
+    }
 }
 
 
@@ -114,8 +143,12 @@ class ManagerEmployee: Employee {
 }
 
 class ContractEmployee: Employee {
-    
+    //Not sure what to put here.
 }
 
 //Vendors
+
+class Vendor: Employee {
+    //Not sure what to put here.
+}
 
